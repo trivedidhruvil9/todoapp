c4f487c141f3fd22baad8d5ce4034c942065d534
Dhruvil Trivedi <trivedidhruvil9@gmail.com>
2020-06-18 07:32:49 +0530

    initial commit

---
+++ assets/js/todo.js
@@ -0,0 +1,27 @@
+// Completing The To-Dos
+$("ul").on("click", "li" ,function(){
+    $(this).toggleClass("completed");
+});
+
+
+// Removing The To-Dos
+$("ul").on("click", "span" ,function(event){
+    $(this).parent().fadeOut(500,function(){
+        $(this).remove();
+    })
+    event.stopPropagation; 
+});
+
+
+// Taking The Input
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        var todoText = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>");
+    }
+});
+
+$(".fa-plus").click(function(){
+    $("input[type='text']").fadeToggle();
+});
